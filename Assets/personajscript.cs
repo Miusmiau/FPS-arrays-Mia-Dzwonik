9a826e50c7732c3dd0b07576ee75d5459eaf9922
Miusmiau <164009952+Miusmiau@users.noreply.github.com>
2024-06-06 08:34:38 -0300

    Initial commit

---
+++ Assets/personajscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personajscript : MonoBehaviour
+{
+    public int nivelHambre;
+    public Alimentoscript alimento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FPS arrays Mia Dzwonik
+ 
